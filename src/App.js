b1ee9064e34c5f8e65c9c3530a159483a4f4c73f
vicmaburrito <vmaldaraca@gmail.com>
2022-01-24 15:05:05 -0600

    Create empty components setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import {
-BrowserRouter as Router,
-Routes,
-Route,
-} from 'react-router-dom'; 
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
+import Rockets from './components/Rockets';
 
-import Header from './components.js/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="mx-5 mt-3">
-      <Header />
-    </div>
-    <Routes>
-        <Route />
+      <div className="mx-5 mt-3">
+        <Header />
+      </div>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/MyProfile" element={<MyProfile />} />
       </Routes>
     </BrowserRouter>
   );

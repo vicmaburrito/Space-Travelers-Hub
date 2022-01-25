08404703b0884b38cd2b935f18eb302a83338c1f
vicmaburrito <vmaldaraca@gmail.com>
2022-01-25 12:59:01 -0600

    Set instead fetch getrocket action to handle fetch

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,12 +1,16 @@
+const GET_ROCKETS = 'GET_ROCKETS';
 const FETCH_ROCKETS = 'Space-Travelers-Hub/rockets/FETCH_ROCKETS';
-const initialState = [];
 
-export const fetchRockets = (payload) => ({
-  type: FETCH_ROCKETS,
-  payload,
-});
+export const getRockets = (dispatch) => {
+  dispatch({ type: GET_ROCKETS });
 
-export const rocketReducer = (state = initialState, action) => {
+  const fetchRockets = (payload) => ({
+    type: FETCH_ROCKETS,
+    payload,
+  });
+};
+
+export const rocketReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ROCKETS:
       return [...state, action.payload];

060a773fed8438fc0a79f28d51543fd77d8447c7
Anjacodes <anja_schmidt7@posteo.de>
2022-01-25 14:11:15 -0600

    Fetch Missions From API and Render to UI

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissionsFromAPI } from '../redux/missions/missions';
+import Mission from './Mission';
+
+import './Missions.css';
 
 function Missions() {
+  const missions = useSelector((state) => state.missions);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (missions.length === 0) dispatch(fetchMissionsFromAPI());
+  }, []);
+
+  const content = missions.map((mission) => (
+    <Mission mission={mission} key={mission.id} />
+  ));
+
   return (
-    <div className="h2 d-flex justify-content-center">Hi from Missions</div>
+    // <div className="h2 d-flex justify-content-center">Hi from Missions</div>
+    <table className="border my-5">
+      <thead>
+        <tr>
+          <th className="border_right border-bottom title p-2">Mission</th>
+          <th className="border_right border-bottom description p-2">Description</th>
+          <th className="border_right border-bottom p-2">Status</th>
+          <th className="border-bottom"> </th>
+        </tr>
+      </thead>
+      <tbody>
+        {content}
+      </tbody>
+    </table>
   );
 }
 

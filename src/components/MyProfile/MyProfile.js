f66719f1c9df824c938adf6b3e42220436d985d3
vicmaburrito <vmaldaraca@gmail.com>
2022-01-26 15:01:01 -0600

    Add reserved rockets in my profile to display it

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './MyProfile.css';
 
 function MyProfile() {
+  const reservedRockets = useSelector(
+    (state) => state.rocketReducer.rockets,
+  ).filter((rocket) => rocket.reserved);
+
   return (
-    // <div className="d-flex">Hi from My Profile</div>
     <div className="d-flex justify-content-between align-items-start reserved-items">
       <div className="data-container">
         <h2>My Missions</h2>
@@ -17,14 +21,13 @@ function MyProfile() {
         </ul>
       </div>
       <div className="data-container">
-        <h2>My Rockets</h2>
+        <h2>My Missions</h2>
         <ul className="data-list p-3">
-          <div className="data-list-container d-flex justify-content-start align-items-center">
-            <li>
-              Rockets 1
-            </li>
-            <button type="submit" className="btn btn btn-primary">Btn</button>
-          </div>
+          {reservedRockets.map((rocket) => (
+            <div className="data-list-container d-flex justify-content-start align-items-center" key={rocket.id}>
+              <li>{rocket.name}</li>
+            </div>
+          ))}
         </ul>
       </div>
     </div>

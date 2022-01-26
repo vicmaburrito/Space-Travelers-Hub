61bae80581d1ac4312b7442b9d79243a941ee108
Anjacodes <anja_schmidt7@posteo.de>
2022-01-26 10:27:43 -0600

    Add Leave Mission Functionality

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { joinMission } from '../redux/missions/missions';
+import JoinButton from './JoinButton';
 
 import './Missions.css';
 
 const Mission = (mission) => {
-  const dispatch = useDispatch();
-
-  const handleClick = (id) => {
-    dispatch(joinMission(id));
-  };
-
   const element = mission.mission; /* eslint-disable-line */
+  console.log(element);
 
   return (
     <tr className="body_row">
       <td className="p-2 border_right border-bottom bold">{element.name}</td>
       <td className="p-2 border_right border-bottom">{element.description}</td>
       <td className="p-2 border_right border-bottom text-center"><span>NOT A MEMBER</span></td>
-      <td className="p-2 border-bottom text-center"><button className="rounded px-2" type="button" onClick={() => handleClick(element.id)}>Join Mission</button></td>
+      <td className="p-2 border-bottom text-center"><JoinButton reserved={element.reserved} id={element.id} /></td>
     </tr>
   );
 };
